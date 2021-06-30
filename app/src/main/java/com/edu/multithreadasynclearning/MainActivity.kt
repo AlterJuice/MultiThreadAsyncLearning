090eae706679b02e36ae51bd5e5566f041b19171
AlterJuice <bogdan171000@gmail.com>
2021-06-30 16:53:56 +0300

    adding RxJava to project Some tests with interval and subscribers

--- app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
+++ app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
@@ -2,10 +2,20 @@ package com.edu.multithreadasynclearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.Flow
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Observable.interval(1, 1, TimeUnit.SECONDS).subscribe(::logText)
+    }
+    private fun logText(text: Long){
+        Log.d("TestLog", text.toString())
     }
 }
