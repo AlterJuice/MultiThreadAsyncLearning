6e029ad0df4d3e50216be28c9a2d132656e6d5e1
AlterJuice <bogdan171000@gmail.com>
2021-07-01 15:14:48 +0300

    Animated circle related to time changes (Seconds and Milliseconds)

--- app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
+++ app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
@@ -6,7 +6,6 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.edu.multithreadasynclearning.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.ObservableEmitter
 import java.util.concurrent.TimeUnit
 
 
@@ -17,42 +16,47 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       // val animation = CircleAnimation(binding.circle, 240)
-       // animation.setDuration(1000)
-       // binding.circle.startAnimation(animation)
+        // val animation = CircleAnimation(binding.circle, 240)
+        // animation.setDuration(1000)
+        // binding.circle.startAnimation(animation)
 
-        Observable.create { e: ObservableEmitter<Any?> ->
-            e.setCancellable { disableButton(binding.buttonPause) }
-            binding.buttonPause.setOnClickListener { v -> e.onNext(v) }
-        }
+        // Observable.create { e: ObservableEmitter<Any?> ->
+        //     e.setCancellable { disableButton(binding.buttonPause) }
+        //     binding.buttonPause.setOnClickListener { v -> e.onNext(v) }
+        // }
 
     }
 
-    private fun disableButton(view: View){
+    private fun disableButton(view: View) {
         view.setOnClickListener(null)
         view.isEnabled = false
     }
 
     override fun onStart() {
         super.onStart()
+        // testThreads {
+        //     Observable.interval(0, 1, TimeUnit.SECONDS).subscribe {
+        //         runOnUiThread{
+        //             binding.circle.updateAngle(((it % 60)*6f).toLong())
+        //         }
+        //     }
+        // }
         testThreads {
-            Observable.interval(0, 1, TimeUnit.SECONDS).subscribe {
-                runOnUiThread{binding.circle.updateAngle((((it*10) % 60)*6f).toLong())}
-                binding.secondsText.text = it.toString()
-            }
-        }
-        testThreads {
-            Observable.interval(0, 1, TimeUnit.MILLISECONDS).subscribe {
-                // runOnUiThread{binding.circle.updateAngle((((it/ 100) % 60000) * 0.6f).toLong())}
-                binding.millisecondsText.text = it.toString()
-            }
+            Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+                .subscribe {
+                    runOnUiThread {
+                        binding.circle.updateAngle(((it * 0.006f) % 360).toLong())
+                        binding.millisecondsText.text = it.toString()
+                    }
+                }
         }
     }
-    private fun logText(text: Long){
+
+    private fun logText(text: Long) {
         Log.d("TestLog", text.toString())
     }
 
-    private fun testThreads(runnable: Runnable){
+    private fun testThreads(runnable: Runnable) {
         Thread(runnable).start()
     }
 }
