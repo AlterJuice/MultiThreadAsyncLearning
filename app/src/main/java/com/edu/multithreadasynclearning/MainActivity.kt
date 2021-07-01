81b103c7b3e3286f946d13c9814035a3121e3ca7
AlterJuice <bogdan171000@gmail.com>
2021-07-01 16:57:09 +0300

    Some changes in MainActivity.kt. Adding TextView Style to project to avoid code repeats.

--- app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
+++ app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
@@ -4,26 +4,48 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import com.edu.multithreadasynclearning.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val liveInteger: MutableLiveData<Long> = MutableLiveData()
+
+    private lateinit var randomDisposable: Disposable
+
+    // private lateinit var circleDisposable: Observable<Long>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        liveInteger.observe(this, {
+            binding.randomIntegerValue.text = it.toString()
+        })
+
+        // circleDisposable = Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+        randomDisposable = Observable.interval(0, 3, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .doOnNext {
+                liveInteger.postValue(getRandomInteger(0, 20000))
+            }
+            .doOnError { Log.d("Error", "?") }
+            .subscribe()
+
         // val animation = CircleAnimation(binding.circle, 240)
         // animation.setDuration(1000)
         // binding.circle.startAnimation(animation)
 
-        // Observable.create { e: ObservableEmitter<Any?> ->
-        //     e.setCancellable { disableButton(binding.buttonPause) }
-        //     binding.buttonPause.setOnClickListener { v -> e.onNext(v) }
-        // }
+
+    }
+    private fun getRandomInteger(intMin: Int, intMax: Int): Long{
+        return (Random.nextInt((intMax - intMin)+1) + intMin).toLong()
 
     }
 
@@ -42,13 +64,12 @@ class MainActivity : AppCompatActivity() {
         //     }
         // }
         testThreads {
-            Observable.interval(0, 1, TimeUnit.MILLISECONDS)
-                .subscribe {
-                    runOnUiThread {
-                        binding.circle.updateAngle((it * 0.006f) % 360)
-                        binding.millisecondsText.text = it.toString()
-                    }
+            Observable.interval(0, 1, TimeUnit.MILLISECONDS).subscribe {
+                runOnUiThread {
+                    binding.circle.updateAngle((it * 0.006f) % 360)
+                    binding.millisecondsText.text = it.toString()
                 }
+            }
         }
     }
 
