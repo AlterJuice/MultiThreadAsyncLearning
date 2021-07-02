ccf4e287f3cd2822967eef2da406576c9e05ab1d
AlterJuice <bogdan171000@gmail.com>
2021-07-02 10:44:13 +0300

    Adding MainScope().launch{} to animate circle

--- app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
+++ app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
@@ -3,12 +3,17 @@ package com.edu.multithreadasynclearning
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import com.edu.multithreadasynclearning.databinding.ActivityMainBinding
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.*
+import java.lang.Runnable
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
@@ -17,64 +22,61 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val liveInteger: MutableLiveData<Long> = MutableLiveData()
 
-    private lateinit var randomDisposable: Disposable
-
-    // private lateinit var circleDisposable: Observable<Long>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        attachObserverToLiveData()
+        runRandomIntegerObservable()
+        runCircleTimeObservable()
+    }
+
+
+    private fun attachObserverToLiveData() {
         liveInteger.observe(this, {
             binding.randomIntegerValue.text = it.toString()
         })
+    }
 
-        // circleDisposable = Observable.interval(0, 1, TimeUnit.MILLISECONDS)
-        randomDisposable = Observable.interval(0, 3, TimeUnit.SECONDS)
-            .subscribeOn(Schedulers.io())
+    private fun runCircleTimeObservable() {
+        Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+            .toFlowable(BackpressureStrategy.DROP)
             .doOnNext {
-                liveInteger.postValue(getRandomInteger(0, 20000))
-            }
-            .doOnError { Log.d("Error", "?") }
-            .subscribe()
-
-        // val animation = CircleAnimation(binding.circle, 240)
-        // animation.setDuration(1000)
-        // binding.circle.startAnimation(animation)
-
-
-    }
-    private fun getRandomInteger(intMin: Int, intMax: Int): Long{
-        return (Random.nextInt((intMax - intMin)+1) + intMin).toLong()
-
-    }
-
-    private fun disableButton(view: View) {
-        view.setOnClickListener(null)
-        view.isEnabled = false
-    }
-
-    override fun onStart() {
-        super.onStart()
-        // testThreads {
-        //     Observable.interval(0, 1, TimeUnit.SECONDS).subscribe {
-        //         runOnUiThread{
-        //             binding.circle.updateAngle(((it % 60)*6f).toLong())
-        //         }
-        //     }
-        // }
-        testThreads {
-            Observable.interval(0, 1, TimeUnit.MILLISECONDS).subscribe {
-                runOnUiThread {
+                MainScope().launch {
                     binding.circle.updateAngle((it * 0.006f) % 360)
                     binding.millisecondsText.text = it.toString()
                 }
             }
+            .doOnError { Log.d("ObservableToFlowable", "Error in Update circle") }
+            .onBackpressureDrop {
+                runOnUiThread {
+                    Toast.makeText(
+                        applicationContext,
+                        "BackpressureStrategy Dropped: $it",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            .subscribe()
+    }
+
+    private fun runRandomIntegerObservable() {
+        testThreads {
+            Observable.interval(0, 3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .doOnNext {
+                    runOnUiThread {
+                        liveInteger.postValue(getRandomInteger(-1000, 1000))
+                    }
+                }
+                .doOnError { runOnUiThread { Log.d("Error", "?") } }
+                .subscribe()
         }
     }
 
-    private fun logText(text: Long) {
-        Log.d("TestLog", text.toString())
+    private fun getRandomInteger(intMin: Int, intMax: Int): Long {
+        return (Random.nextInt((intMax - intMin) + 1) + intMin).toLong()
     }
 
     private fun testThreads(runnable: Runnable) {
