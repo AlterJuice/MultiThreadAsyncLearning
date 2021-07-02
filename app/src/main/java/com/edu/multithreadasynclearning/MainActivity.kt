f0547755bfe1aba00b9f4325718d34a81d4a2ded
AlterJuice <bogdan171000@gmail.com>
2021-07-02 14:59:04 +0300

    Using threads, coroutines and RxJava.
    Threads, Observable/Flowable, Coroutines -> three different things.
    Observable and Flowable are almost the same with one difference -> Flowable supports backpressure
    Do not mix&use one within another one
    flow {} (coroutines) (Kotlin extension funcs); after you can use @collect method.
    Do not use coroutines scope in threads (ya?)

--- app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
+++ app/src/main/java/com/edu/multithreadasynclearning/MainActivity.kt
@@ -2,18 +2,18 @@ package com.edu.multithreadasynclearning
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
 import com.edu.multithreadasynclearning.databinding.ActivityMainBinding
-import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.*
-import java.lang.Runnable
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import java.time.LocalTime
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
@@ -22,64 +22,124 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val liveInteger: MutableLiveData<Long> = MutableLiveData()
 
+    private var thread: Thread? = null
+    private var job: Job? = null
+
+    private var disposableTime: Disposable? = null
+
+
+    /*
+    * SubscribeOn specify the Scheduler on which an Observable will operate.
+    * ObserveOn specify the Scheduler on which an observer will observe this Observable.
+    * So basically SubscribeOn is mostly subscribed (executed) on a background thread
+    * ( you do not want to block the UI thread while waiting for the observable)
+    * and also in ObserveOn you want to observe the result on a main thread...
+    * If you are familiar with AsyncTask then SubscribeOn is similar
+    *  to doInBackground method and ObserveOn to onPostExecute...
+    * */
+
+    // Threads, Observable/Flowable, Coroutines -> three different things.
+    // Observable and Flowable are almost the same with one difference -> Flowable supports backpressure
+    // Do not mix&use one within another one
+    // flow {} (coroutines) (Kotlin extension funcs); after you can use @collect method.
+    // Do not use coroutines scope in threads (ya?)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        attachObserverToLiveData()
-        runRandomIntegerObservable()
-        runCircleTimeObservable()
+        attachLiveDataObserver()
+        startThread()
+        startRxFlowable()
+        startJobCoroutines()
     }
 
 
-    private fun attachObserverToLiveData() {
+    private fun attachLiveDataObserver() {
         liveInteger.observe(this, {
-            binding.randomIntegerValue.text = it.toString()
+            binding.randomIntegerThreadValue.text = it.toString()
         })
     }
 
-    private fun runCircleTimeObservable() {
-        Observable.interval(0, 1, TimeUnit.MILLISECONDS)
-            .toFlowable(BackpressureStrategy.DROP)
-            .doOnNext {
-                MainScope().launch {
-                    binding.circle.updateAngle((it * 0.006f) % 360)
-                    binding.millisecondsText.text = it.toString()
+    private fun startJobCoroutines() {
+        job = MainScope().launch {
+            coroutine().collect {
+                withContext(Dispatchers.Main) {
+                    binding.randomIntegerCoroutinesValue.text = it.toString()
+//                    liveInteger.value = it
                 }
             }
-            .doOnError { Log.d("ObservableToFlowable", "Error in Update circle") }
-            .onBackpressureDrop {
-                runOnUiThread {
-                    Toast.makeText(
-                        applicationContext,
-                        "BackpressureStrategy Dropped: $it",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-            .subscribe()
-    }
-
-    private fun runRandomIntegerObservable() {
-        testThreads {
-            Observable.interval(0, 3, TimeUnit.SECONDS)
-                .subscribeOn(Schedulers.io())
-                .doOnNext {
-                    runOnUiThread {
-                        liveInteger.postValue(getRandomInteger(-1000, 1000))
-                    }
-                }
-                .doOnError { runOnUiThread { Log.d("Error", "?") } }
-                .subscribe()
         }
     }
 
-    private fun getRandomInteger(intMin: Int, intMax: Int): Long {
-        return (Random.nextInt((intMax - intMin) + 1) + intMin).toLong()
+    private fun startThread() {
+        thread = createThread()
     }
 
-    private fun testThreads(runnable: Runnable) {
-        Thread(runnable).start()
+    private fun startRxFlowable() {
+        // Main process callbacks are onNext and onError in subscribe
+
+        // Проблема с Flowable: с методом .onBackpressureDrop() анимация и текст
+        // меняется раз в 3 секунды (рывками) и переодически вылетает значение из ErrorReturn блока.
+        // Без .onBackpressureDrop() интерфейс не обновляется
+
+        disposableTime = Observable.interval(0, 1, TimeUnit.MILLISECONDS)
+            .onErrorReturn {
+                -1
+            }
+            // .onBackpressureDrop()
+            .delay(5, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                binding.circle.updateAngle((it * 0.006f) % 360)
+                binding.millisecondsText.text = getFactTimeFormatted3parts(it / 1000 )
+                // binding.millisecondsText.text = it.toString()
+
+            }, {
+                Log.d("An error occurred: ", it.toString())
+            })
+    }
+
+
+    private fun createThread(): Thread {
+        val t = Thread {
+            while (thread?.isInterrupted != true) {
+                liveInteger.postValue(getRandomInteger(-1000, 1000))
+                Thread.sleep(3000)
+            }
+        }
+        t.start()
+        return t
+
+    }
+
+    private fun coroutine() = flow<Long> {
+        repeat(1000){
+            delay(1000)
+            emit(getRandomInteger(0, 30_000))
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        thread?.interrupt()
+        disposableTime?.dispose()
+        job?.cancel()
+        Log.d("OnDestroy", "Thread, disposableTime and job destroyed")
+
+    }
+
+
+    private fun getFactTimeFormatted3parts(factTime: Long): String {
+        val formatted = LocalTime.ofSecondOfDay(factTime).toString()
+        return if (formatted.startsWith("00:")) formatted.substring(3) else formatted
+    }
+
+
+    private fun getRandomInteger(intMin: Int, intMax: Int): Long {
+        return (Random.nextInt((intMax - intMin) + 1) + intMin).toLong()
     }
 }
